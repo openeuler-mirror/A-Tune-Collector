f9f6c4506bc1ede1359163ca069facfd32823b7b
hanxinke <hanxinke@huawei.com>
2020-11-16 20:36:28 +0800

    atune_collector: Initialize the collection and configuration capability.

---
+++ atune_collector/collect_data.py
@@ -0,0 +1,90 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# Copyright (c) 2020 Huawei Technologies Co., Ltd.
+# A-Tune is licensed under the Mulan PSL v2.
+# You can use this software according to the terms and conditions of the Mulan PSL v2.
+# You may obtain a copy of Mulan PSL v2 at:
+#     http://license.coscl.org.cn/MulanPSL2
+# THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR
+# PURPOSE.
+# See the Mulan PSL v2 for more details.
+# Create: 2020-11-13
+
+"""
+The main function for collecting data.
+"""
+import argparse
+import json
+import os
+import time
+
+from plugin.plugin import MPI
+
+
+class Collector:
+    """class for Collector"""
+
+    def __init__(self, data):
+        self.data = data
+        self.field_name = []
+
+    def parse_json(self):
+        """parse json data"""
+        monitors = []
+        for item in self.data["collection_items"]:
+            parameters = []
+            parameters.append("--interval=%s;" % self.data["interval"])
+            for metric in item["metrics"]:
+                self.field_name.append("%s.%s.%s" % (item["module"], item["purpose"], metric))
+                parameters.append("--fields=%s" % metric)
+            if "threshold" in item:
+                parameters.append("--threshold=%s" % item["threshold"])
+            parameters.append("--nic=%s" % self.data["network"])
+            parameters.append("--device=%s" % self.data["block"])
+            monitors.append([item["module"], item["purpose"], " ".join(parameters)])
+        return monitors
+
+    def save_csv(self, field_data):
+        """save data to csv file"""
+        path = self.data["output_dir"]
+        if not os.path.exists(path):
+            os.makedirs(path, 0o750)
+        file_name = "{}-{}.csv".format(self.data.get("workload_type", "default"),
+                                       int(round(time.time() * 1000)))
+        import pandas as pd
+        writer = pd.DataFrame(columns=self.field_name, data=field_data)
+        writer.to_csv(os.path.join(path, file_name), encoding='utf-8', index=False)
+        print("finish to collect data, csv path is %s" % os.path.join(path, file_name))
+
+    def collect_data(self):
+        """collect data"""
+        collect_num = self.data["sample_num"]
+        if int(collect_num) < 1:
+            os.abort("sample_num must be greater than 0")
+        field_data = []
+        monitors = self.parse_json()
+        print(" ".join(self.field_name))
+        for _ in range(collect_num):
+            raw_data = MPI.get_monitors_data(monitors)
+            float_data = list()
+            for num in raw_data:
+                float_data.append(float(num))
+
+            str_data = [str(round(value, 3)) for value in float_data]
+            print(" ".join(str_data))
+            field_data.append(float_data)
+        return field_data
+
+
+if __name__ == "__main__":
+    default_json_path = "/etc/atune_collector/collect_data.json"
+    ARG_PARSER = argparse.ArgumentParser(description="input configuration file in json format")
+    ARG_PARSER.add_argument('-c', '--config', metavar='json',
+                            default=default_json_path, help='input json path')
+    ARGS = ARG_PARSER.parse_args()
+    with open(ARGS.config, 'r') as file:
+        json_data = json.load(file)
+    collector = Collector(json_data)
+    dataset = collector.collect_data()
+    collector.save_csv(dataset)
